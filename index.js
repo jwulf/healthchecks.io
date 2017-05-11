fe88265c63fa9ce98b6c8cd9b577acad0e759732
Joshua Wulf <sitapati@worldsankirtan.net>
2017-05-11 22:15:27 +1000

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const https = require('https');
+
+module.exports = function healthcheck(url, schedule) {
+	https.get(url);
+	return setInterval(function check() {
+		https.get(url);
+	}, schedule * 1000);
+}
