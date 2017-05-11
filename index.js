4a18406bbc78f213e5d533dfb2b6913a0b4e6108
Joshua Wulf <sitapati@worldsankirtan.net>
2017-05-11 22:16:52 +1000

    schedule is now minutes

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ module.exports = function healthcheck(url, schedule) {
 	https.get(url);
 	return setInterval(function check() {
 		https.get(url);
-	}, schedule * 1000);
+	}, schedule * 60 * 1000);
 }
