7a684a15147a12483d90f0a8bd31718a71e4e08e
Joshua Wulf <sitapati@worldsankirtan.net>
2017-05-11 22:21:46 +1000

    default to 30 mins, add debug

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const https = require('https');
+var https = require('https');
+var debug = require('debug')('healthcheck');
 
 module.exports = function healthcheck(url, schedule) {
+	if (!schedule || isNaN(parseInt(schedule))) {
+		schedule = 30;
+	}
+	var time = schedule * 60 * 1000;
+	debug('Set up healthchecks.io on ' + url + ' every ' + time + ' minutes.'; 
 	https.get(url);
 	return setInterval(function check() {
 		https.get(url);
