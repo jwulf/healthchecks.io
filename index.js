9ce343755cd07a1a62218e486c666dcc8cba9cfd
Joshua Wulf <sitapati@worldsankirtan.net>
2017-05-22 18:25:51 +1000

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var https = require('https');
 var debug = require('debug')('healthcheck');
 
 module.exports = function healthcheck(url, schedule) {
+	if (!url) {
+		console.log('No URL provided for healthcheck.io');
+		return;
+	}
 	if (!schedule || isNaN(parseInt(schedule))) {
 		schedule = 30;
 	}
