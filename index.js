6be5c64d36a8375d5d2913db85f6b49d5fa37c22
Joshua Wulf <sitapati@worldsankirtan.net>
2017-06-08 02:15:38 +1000

    Wrap http.get in try-catch block

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-var https = require('https');
-var debug = require('debug')('healthcheck');
-
-module.exports = function healthcheck(url, schedule) {
-	if (!url) {
-		console.log('No URL provided for healthcheck.io');
-		return;
-	}
-	if (!schedule || isNaN(parseInt(schedule))) {
-		schedule = 30;
-	}
-	debug('Set up healthchecks.io on ' + url + ' every ' + schedule + ' minutes.'); 
-	https.get(url);
-	return setInterval(function check() {
-		https.get(url);
-	}, schedule * 60 * 1000);
-}
+module.exports = require('./lib/healthcheck');
