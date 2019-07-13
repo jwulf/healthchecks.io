38792cc95039f482ccbc48b17f29389566a7d991
Josh Wulf <josh@magikcraft.io>
2019-07-13 14:05:37 +1000

    Support http endpoints. Fixes #1

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,28 @@
-import * as _debug from 'debug'
-import * as https from 'https'
+import * as _debug from "debug";
+import * as http from "http";
+import * as https from "https";
 
-const debug = _debug('healthcheck');
+const debug = _debug("healthcheck");
 
 function healthcheck(url: string, schedule = 30) {
-	const check = url => () => { try { https.get(url) } catch (e) { } }
+  const httpLib = url.indexOf("https://") === 0 ? https : http;
+  const check = url => () => {
+    try {
+      httpLib.get(url);
+    } catch (e) {}
+  };
 
-	if (!url) {
-		console.log('No URL provided for healthcheck.io')
-		return null
-	}
-	if (!schedule || isNaN(parseInt(schedule.toString()))) {
-		schedule = 30
-	}
-	debug(`Set up healthchecks.io on ${url} every ${schedule} minutes.`)
-	const heartbeat = check(url)
-	heartbeat()
-	return setInterval(heartbeat, schedule * 60 * 1000)
+  if (!url) {
+    console.log("No URL provided for healthcheck.io");
+    return null;
+  }
+  if (!schedule || isNaN(parseInt(schedule.toString()))) {
+    schedule = 30;
+  }
+  debug(`Set up healthchecks.io on ${url} every ${schedule} minutes.`);
+  const heartbeat = check(url);
+  heartbeat();
+  return setInterval(heartbeat, schedule * 60 * 1000);
 }
 
-export = healthcheck
+export = healthcheck;
